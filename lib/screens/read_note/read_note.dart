a9184bc6a1080a474a80d5600e50c03b4b789436
Trịnh Xuân Đạt <datbmtt1@gmail.com>
2021-01-05 16:23:58 +0700

    up

--- lib/screens/read_note/read_note.dart
+++ lib/screens/read_note/read_note.dart
@@ -101,6 +101,7 @@ class _EditNoteState extends State<EditNote> {
                 if (notes.title != '' && notes.body != '') {
                   await data.updateNote(http.Client(), params);
                   Navigator.pop(context);
+                  data.update();
                 }
               },
             ),
