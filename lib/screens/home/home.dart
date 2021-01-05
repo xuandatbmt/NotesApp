a7f5c8ba964fe4e071ae1df8157f9dc8d4a2155d
Pham Duy <phd051199@gmail.com>
2021-01-05 17:08:10 +0700

    updated

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -36,7 +36,7 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(
           "Notes Manager",
           textAlign: TextAlign.right,
-          style: TextStyle(color: textColor),
+          style: TextStyle(color: Color(0xff40b3fb)),
         ),
         leading: FlatButton(
             onPressed: () {
